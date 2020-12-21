eea3655b43b323f29b3c9db0a90484178d124793
Muzi <lytasia@outlook.fr>
2020-12-21 21:06:56 +0100

    Deleted .old
    because it's useless now

--- .old/BrowserPlugin/monikai.js
+++
@@ -1,7 +0,0 @@
-browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.url) {
-        var http = new XMLHttpRequest();
-        http.open("POST", "http:/localhost:9812/", true);
-        http.send(changeInfo.url);
-    }
-});
